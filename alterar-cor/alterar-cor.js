48f678f6758e055e9691ad89d3a2c39ce6a29dfb
OtavioGondim <otaviogondim31@gmail.com>
2021-10-11 18:08:41 -0300

    Novo aprendizado

---
+++ alterar-cor/alterar-cor.js
@@ -0,0 +1,15 @@
+document.getElementById("btn-azul").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "azul")
+})
+
+document.getElementById("btn-verde").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "verde")
+})
+
+document.getElementById("btn-preto").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "preto")
+})
+
+document.getElementById("btn-roxo").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class", "roxo")
+})
